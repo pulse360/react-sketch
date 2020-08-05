add80190ef83087e6931787c5024110c3a051cda
Santiago Persico <santiagopersico@gmail.com>
2020-08-05 01:04:24 -0300

    update and fix deps

--- src/UI/Slider/index.js
+++ src/UI/Slider/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './styles.css'
-import Slider from '@material-ui/core/Slider';
+import Slider from '@material-ui/lab/Slider'
 import { Tooltip } from '@material-ui/core'
 
 class SliderComponent extends React.Component {
@@ -15,17 +15,16 @@ class SliderComponent extends React.Component {
   }
 
   render() {
-
     return (
-      <Tooltip title={"Stroke width: " + this.props.value} arrow>      
-      <div className='slider-wrapper'>
-        <svg width='20' height='20' viewBox='0 0 20 20' fill='none' xmlns='http://www.w3.org/2000/svg'>
-          <path d='M11 1L10 0L0 10L1 11L11 1Z' fill='black' />
-          <path d='M15 5L13 3L3 13L5 15L15 5Z' fill='black' />
-          <path d='M10 20L7 17L17 7L20 10L10 20Z' fill='black' />
-        </svg>
-        <div className='input-wrapper'>
-          {/* <svg
+      <Tooltip title={'Stroke width: ' + this.props.value} arrow>
+        <div className='slider-wrapper'>
+          <svg width='20' height='20' viewBox='0 0 20 20' fill='none' xmlns='http://www.w3.org/2000/svg'>
+            <path d='M11 1L10 0L0 10L1 11L11 1Z' fill='black' />
+            <path d='M15 5L13 3L3 13L5 15L15 5Z' fill='black' />
+            <path d='M10 20L7 17L17 7L20 10L10 20Z' fill='black' />
+          </svg>
+          <div className='input-wrapper'>
+            {/* <svg
             className='track-line'
             width='198'
             height='5'
@@ -36,18 +35,17 @@ class SliderComponent extends React.Component {
           >
             <path d='M2.5 2.5L198 0L198 5Z' fill='black' />
           </svg> */}
-          <Slider
-            step={1} min={1} max={50}
-            aria-labelledby="slider"
-            value={this.props.value}
-            onChange={(e, v) =>
-              this.props.onChange(v)
-            }
-          />
+            <Slider
+              step={1}
+              min={1}
+              max={50}
+              aria-labelledby='slider'
+              value={this.props.value}
+              onChange={(e, v) => this.props.onChange(v)}
+            />
+          </div>
         </div>
-      </div>
       </Tooltip>
-
     )
   }
 }
