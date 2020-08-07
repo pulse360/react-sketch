7d552c062701d09cf6ec1bddc2864ae37bd80d4c
Santiago Persico <santiagopersico@gmail.com>
2020-08-07 17:09:39 -0300

    better data loading testing setup

--- examples/main.jsx
+++ examples/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { SketchBoard } from '../src/index'
 
+// import {data} from './dumyData' // dummy data usage
+
+const dummyData = {
+  id: null,
+  json: { objects: [], heightFactor: 5 },
+  heightFactor: 5
+}
+
 class SketchFieldDemo extends React.Component {
   render() {
-    return <SketchBoard />
+    return <SketchBoard defaultValue={dummyData.json} />
   }
 }
 
