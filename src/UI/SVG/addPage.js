763c75f951273f9d40550e4e8a4855c9b9a90f43
Santiago Persico <santiagopersico@gmail.com>
2020-08-14 17:48:00 -0300

    add new buttons

--- src/UI/SVG/addPage.js
+++ src/UI/SVG/addPage.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const AddCircleIcon = () => (
     <svg width='24' height='24' viewBox='0 0 24 24' fill='#20A0FF' xmlns='http://www.w3.org/2000/svg'>
-        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"></path>    </svg>
+        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"></path>    
+   </svg>
   )
   
 export default AddCircleIcon
---
+++ src/UI/SVG/arrowUp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ArrowUpIcon = () => (
+  <svg width='24' height='24' viewBox='0 0 24 24' fill='#20A0FF' xmlns='http://www.w3.org/2000/svg'>
+    <path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"></path>
+  </svg>
+)
+
+export default ArrowUpIcon
