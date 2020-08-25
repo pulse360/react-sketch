3d08cdcedd21cda63b258e5e096ce5b2ec6b3aa3
Santiago Persico <santiagopersico@gmail.com>
2020-08-24 21:24:06 -0300

    moves the navigation toolbar to the bottom right

--- src/UI/ToolsPanel/index.js
+++ src/UI/ToolsPanel/index.js
@@ -23,91 +23,88 @@ import { Tooltip } from '@material-ui/core'
 const ToolsPanel = ({ selectTool, addImage, selectedTool, scrollUp, scrollDown, addPage }) => {
   return (
     <>
-    <div className='left-toolbar'>
-      <IconButton onClick={() => selectTool('select')} selectedTool={selectedTool} tool='select' title='Select'>
-        <SelectIcon />
-      </IconButton>
-      <IconButton onClick={() => selectTool('pencil')} selectedTool={selectedTool} tool='pencil' title='Pencil'>
-        <PencilIcon />
-      </IconButton>
-      <IconButton onClick={() => selectTool('arrow')} selectedTool={selectedTool} tool='arrow' title='Arrow'>
-        <ArrowIcon />
-      </IconButton>
-      <IconButton onClick={() => selectTool('line')} selectedTool={selectedTool} tool='line' title='Line'>
-        <LineIcon />
-      </IconButton>
-      <IconButton
-        onClick={() => selectTool('highlighter')}
-        selectedTool={selectedTool}
-        tool='highlighter'
-        title='Hightlight'
-      >
-        <HighlighterIcon />
-      </IconButton>
-      <IconButton
-        onClick={() => selectTool('rectangle')}
-        selectedTool={selectedTool}
-        tool='rectangle'
-        title='Rectangle'
-      >
-        <RectangleIcon />
-      </IconButton>
-      <IconButton onClick={() => selectTool('circle')} selectedTool={selectedTool} tool='circle' title='Circle'>
-        <CircleleIcon />
-      </IconButton>
-      <IconButton onClick={() => selectTool('text')} selectedTool={selectedTool} tool='text' title='Add Text'>
-        <AddTextIcon />
-      </IconButton>
-      {/* <IconButton onClick={addText}>
-        <AddTextIcon />
-      </IconButton> */}
-      <Tooltip title='Add Image' placement='right'>
-        <DropZone
-          accept='image/*'
-          multiple={false}
-          className='drop-area-images'
-          onDrop={(file) => {
-            const reader = new FileReader()
-            reader.onloadend = () => {
-              const b64 = reader.result
-              addImage(b64)
-            }
-
-            reader.readAsDataURL(file[0])
-          }}
+      <div className='sketch-toolbar left'>
+        <IconButton onClick={() => selectTool('select')} selectedTool={selectedTool} tool='select' title='Select'>
+          <SelectIcon />
+        </IconButton>
+        <IconButton onClick={() => selectTool('pencil')} selectedTool={selectedTool} tool='pencil' title='Pencil'>
+          <PencilIcon />
+        </IconButton>
+        <IconButton onClick={() => selectTool('arrow')} selectedTool={selectedTool} tool='arrow' title='Arrow'>
+          <ArrowIcon />
+        </IconButton>
+        <IconButton onClick={() => selectTool('line')} selectedTool={selectedTool} tool='line' title='Line'>
+          <LineIcon />
+        </IconButton>
+        <IconButton
+          onClick={() => selectTool('highlighter')}
+          selectedTool={selectedTool}
+          tool='highlighter'
+          title='Hightlight'
         >
-          <PastImageIcon />
-        </DropZone>
-      </Tooltip>
-      <IconButton
-        onClick={() => selectTool('eraser')}
-        selectedTool={selectedTool}
-        tool='eraser'
-        title='Element Eraser'
-      >
-        <EraserIcon />
-      </IconButton>
-    </div>
-    <div className="left-toolbar navigation">
-    <IconButton
-        onClick={scrollUp}
-        title='Scroll Up'
-      >
-        <ArrowUpIcon />
-      </IconButton>
-      <IconButton
-        onClick={scrollDown}
-        title='Scroll Down'
-      >
-        <ArrowDownIcon />
-      </IconButton>
-      <IconButton 
-        onClick={addPage} 
-        title='Add new page'
-      >
-        <AddCircleIcon />
-      </IconButton>
-    </div>
+          <HighlighterIcon />
+        </IconButton>
+        <IconButton
+          onClick={() => selectTool('rectangle')}
+          selectedTool={selectedTool}
+          tool='rectangle'
+          title='Rectangle'
+        >
+          <RectangleIcon />
+        </IconButton>
+        <IconButton onClick={() => selectTool('circle')} selectedTool={selectedTool} tool='circle' title='Circle'>
+          <CircleleIcon />
+        </IconButton>
+        <IconButton onClick={() => selectTool('text')} selectedTool={selectedTool} tool='text' title='Add Text'>
+          <AddTextIcon />
+        </IconButton>
+        <Tooltip title='Add Image' placement='right'>
+          <DropZone
+            accept='image/*'
+            multiple={false}
+            className='drop-area-images'
+            onDrop={(file) => {
+              const reader = new FileReader()
+              reader.onloadend = () => {
+                const b64 = reader.result
+                addImage(b64)
+              }
+
+              reader.readAsDataURL(file[0])
+            }}
+          >
+            <PastImageIcon />
+          </DropZone>
+        </Tooltip>
+        <IconButton
+          onClick={() => selectTool('eraser')}
+          selectedTool={selectedTool}
+          tool='eraser'
+          title='Element Eraser'
+        >
+          <EraserIcon />
+        </IconButton>
+      </div>
+      <div className="sketch-toolbar right">
+        <IconButton
+          onClick={scrollUp}
+          title='Scroll Up'
+        >
+          <ArrowUpIcon />
+        </IconButton>
+        <IconButton
+          onClick={scrollDown}
+          title='Scroll Down'
+        >
+          <ArrowDownIcon />
+        </IconButton>
+        <IconButton 
+          onClick={addPage} 
+          title='Add new page'
+        >
+          <AddCircleIcon />
+        </IconButton>
+      </div>
     </>
   )
 }
